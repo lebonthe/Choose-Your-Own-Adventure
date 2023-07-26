daa7b220a83fe1d476a1e3dc2a5254fa18f545fa
Min Hu <lebonthe@gmail.com>
2023-07-26 16:48:16 +0800

    Initial Commit

---
+++ Choose Your Own Adventure/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Choose Your Own Adventure
+//
+//  Created by Min Hu on 2023/7/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
